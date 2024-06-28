af4c622cf150d7bf3422b71fea0c938fa70c92a2
Alegarcia049 <alegarcia049@gmail.com>
2024-06-28 20:34:26 -0300

    game final stage

--- program/Poke_game.py
+++ program/Poke_game.py
@@ -17,7 +17,6 @@ SIZE = (1060, 596)
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 SCREEN = pg.display.set_mode(SIZE)
-FONT = pg.font.SysFont(None, 55)        #sets fonts for text
 
 KANTO_TEAMS = {
                 "Will": ("Bronzong", "Jynx", "Grumpig", "Slowbro", "Gardevoir", "Xatu"),
@@ -47,9 +46,9 @@ def crear_movimientos(moves: list[str], lista_moves: list)->list[Move]:
                 lista_obj_moves.append(Move(name, type1, category, pp, power, accuaracy))
     return lista_obj_moves 
 
-def crear_pokemon(indice,pokemones: list[str], lista_moves: list)->Pokemon:
+def crear_pokemon(indice)->Pokemon:
     while True:
-        linea = pokemones[indice]
+        linea = POKE_LIST[indice]
         pokedex_number = int(linea[0])
         name = linea[1]
         type1 = linea[2]
@@ -65,26 +64,24 @@ def crear_pokemon(indice,pokemones: list[str], lista_moves: list)->Pokemon:
         weight = float(linea[12]) if len(linea[12]) > 0 else 0 
         is_legendary = bool(int(linea[13]))
         moves = linea[14].split(";")
-        obj_moves = crear_movimientos(moves,lista_moves)
+        obj_moves = crear_movimientos(moves,MOVES)
         level = 50
         
         return Pokemon(pokedex_number, name, type1, type2, hp, attack, deffense, sp_attack, sp_defense, speed, generation, height, weight, is_legendary, obj_moves, level)
 
-def pokemon_por_nombre(nombre: str, pokemones: list[str] = POKE_LIST, lista_moves: list[list[str]]= MOVES)->Pokemon:
-    #Buscar indice
+def pokemon_por_nombre(nombre: str)->Pokemon:
     indice = 0
-    for i in range(len(pokemones)-1):
-        if nombre == pokemones[i][1]:
+    for i in range(len(POKE_LIST)-1):
+        if nombre == POKE_LIST[i][1]:
             indice = i
-    return crear_pokemon(indice, pokemones, lista_moves)
+    return crear_pokemon(indice)
 
-def team_por_nombre(nombre_equipo:str, lista_nombre: list[str], pokemones: list[str] = POKE_LIST, lista_moves: list[list[str]]= MOVES)->Team:
+def team_por_nombre(nombre_equipo:str, lista_poketeam: list[str])->Team:
     equipo = []
-    for nombre in lista_nombre:
+    for nombre in lista_poketeam:
         pokemon = pokemon_por_nombre(nombre)
         equipo.append(pokemon)
     return Team(nombre_equipo, equipo)
-
 
 def reset_screen()->None: 
     """Reffills screen with total black to clear scene and free up space on runtime"""
@@ -104,7 +101,7 @@ def wait_for_no_keys()->None:
     
 def wait_for_key_pressed():
     """
-    It simply pause the program till every single key is not pressed.
+    It simply pause the program till any key is pressed.
     Only then continues running
     """
     while True:
@@ -120,14 +117,28 @@ def check_quit()->None:
     for event in pg.event.get():
         if event.type == pg.QUIT: pg.quit()
 
-def display_message(msg:str, position: tuple[int, int], color: tuple[int, int, int], rescale: float | None = None)->None:
-    message = FONT.render(msg, True, color)
-    if rescale: message = pg.transform.scale_by(message, rescale)
-    message_rect = message.get_rect(center = position)   
+def display_message(msg:str, position: tuple[int, int], color: tuple[int, int, int], size: int = 55, box: bool = False)->None:
+    """
+    Display a message on screen making use of SCREEN (global variable) given:
+    - msg: The message to display.
+    - position: The position (x, y) to center the message.
+    - color: The color of the text.
+    - size: The font size of the text.
+    - box: Whether to display a black rectangle box behind the text.
+    """
+    message = pg.font.SysFont(None, size).render(msg, True, color)
+    message_rect = message.get_rect(center = position)
+    if box:
+        box_rect = message_rect.inflate(20, 20)
+        box_color = (0, 0, 0)
+        pg.draw.rect(SCREEN, box_color, box_rect)
     SCREEN.blit(message, message_rect)
     pg.display.flip()
 
-def rescale_image(image: pg.Surface, target_size: tuple[int, int])->None:
+def rescale_image(image: pg.Surface, target_size: tuple[int, int])->pg.Surface:
+    """
+    Rescales a given image (Surface) and a target_size
+    """
     image_width, image_height = image.get_size()
     image_ratio = image_width / image_height
     target_width, target_height = target_size
@@ -141,6 +152,10 @@ def rescale_image(image: pg.Surface, target_size: tuple[int, int])->None:
     return pg.transform.scale(image, (new_width, new_height))
 
 def draw_bg(name: str)->None:
+    """
+    Draws the image on background given a certain name of the image 
+    (findable it at /data/imgs/simu)
+    """
     reset_screen()
     bg = pg.image.load(os.path.join("data", "imgs", "simu", name))
     bg = rescale_image(bg, SIZE)
@@ -161,9 +176,11 @@ def draw_poke_img(name: str, position: tuple[int, int], rescale: float | None = 
     SCREEN.blit(img, img_rect)
     pg.display.flip()
 
-
 def draw_pokemons_menu(team: Team)->None:
-     for poknum, pokemon in enumerate(team.pokemons):
+    """
+    Displays, over the menu layout the pokemon imgs and names
+    """
+    for poknum, pokemon in enumerate(team.pokemons):
         for i in range(len(POKE_LIST)):
             if POKE_LIST[i][1]==pokemon.name: 
                 img_index = int(POKE_LIST[i][0])
@@ -172,9 +189,14 @@ def draw_pokemons_menu(team: Team)->None:
         elif img_index<100: img_index = f'0{str(img_index)}.png'
         else: img_index = f'{str(img_index)}.png'
         draw_poke_img(img_index, ((1+poknum)*SIZE[0]//7, SIZE[1]//2))
-        display_message(pokemon.name, ((1+poknum)*SIZE[0]//7, SIZE[1]//2+100), WHITE, 0.5)
+        display_message(pokemon.name.upper(), ((1+poknum)*SIZE[0]//7, SIZE[1]//2+100), color=WHITE, size=25)
+        if team.name == 'YOU':
+            display_message(str(poknum+1), ((1+poknum)*SIZE[0]//7, SIZE[1]//2+150), color=WHITE, size=30, box=True)
      
 def draw_poketeam_battleground(team: Team, position_1: tuple[int, int])->None:
+    """
+    Displays, over the battleground layout the pokemon imgs, names and health status
+    """
     for poknum, pokemon in enumerate(team.pokemons):
         for i in range(len(POKE_LIST)):
             if POKE_LIST[i][1]==pokemon.name: 
@@ -195,24 +217,31 @@ def draw_poketeam_battleground(team: Team, position_1: tuple[int, int])->None:
     pg.display.flip()
 
 def draw_move(team1: Team, team2: Team):
+    """
+    Displays the status of the current pokemon and its move
+    """
     wait_for_no_keys()
     next_action = team1.get_next_action(team2, EFFECTIVENESS)
     if next_action[0] == 'attack':
         move = next_action[1]
         damage = move.get_damage(team1.get_current_pokemon(), team2.get_current_pokemon(), EFFECTIVENESS)
-        display_message(move.name.upper(), position=(SIZE[0]//2, SIZE[1]//2-130), color=WHITE)
-        display_message(str(int(damage)), position=(SIZE[0]//2, SIZE[1]//2-80), color=WHITE)
+        display_message(move.name.upper(), position=(SIZE[0]//2, SIZE[1]//2-140), color=WHITE, box=True)
+        display_message(str(int(damage)), position=(SIZE[0]//2, SIZE[1]//2-90), color=WHITE, box=True)
         team1.do_action(next_action[0], next_action[1], team2, EFFECTIVENESS)
     elif next_action[0] == 'switch':
-        display_message('SWITCHING', position=(SIZE[0]//2, SIZE[1]//2-130), color=WHITE)
+        display_message('SWITCHING', position=(SIZE[0]//2, SIZE[1]//2-140), color=WHITE, box=True)
         team1.do_action(next_action[0], next_action[1], team2, EFFECTIVENESS)
     elif next_action[0] == 'skip':
-        display_message('SKIPPING', position=(SIZE[0]//2, SIZE[1]//2-130), color=WHITE)
+        display_message('SKIPPING', position=(SIZE[0]//2, SIZE[1]//2-140), color=WHITE, box=True)
         team1.do_action(next_action[0], next_action[1], team2, EFFECTIVENESS)
     pg.display.flip()
 
      
 def draw_pokemons_health_bar(pokemon: Pokemon,  position: tuple[int, int])->None:
+    """
+    Creates health bars for each pokemon on the battleground
+    that is then used to display health status
+    """
     max_hp = pokemon.max_hp
     current_hp = pokemon.current_hp
     bar_length = (max_hp / pokemon.max_hp) * 100
@@ -228,6 +257,9 @@ def draw_pokemons_health_bar(pokemon: Pokemon,  position: tuple[int, int])->None
     pg.display.flip()
 
 def menu()->None:
+    """
+    Enviroment for menu, display and things to be drawn
+    """
     reset_screen()
     draw_bg('menu_bg.jpg')
     while True:
@@ -238,12 +270,17 @@ def menu()->None:
                 if event.type == pg.KEYDOWN: return None
 
 def select_kanto_champs()->str:
+    """
+    Enviroment for selecting the oponent, display and things to be drawn
+    """
     reset_screen()
     draw_bg('kanto_champions.jpg')
-    display_message("SELECT YOUR OPPONENT", (SIZE[0]//2, SIZE[1]//2), color=WHITE)
+    display_message("SELECT YOUR OPPONENT", (SIZE[0]//2, SIZE[1]-100), color=WHITE, size=100, box=True)
+    for i in range(1, 6):
+        display_message(str(i), (i*SIZE[0]//6, 100), color=WHITE, size=100, box=True)
     wait_for_no_keys()
     selected = None
-    while not selected:         #select kanto champion to fight against
+    while not selected:        
         key_pressed = pg.key.get_pressed()
         if key_pressed[pg.K_1]: selected = 'Will'
         elif key_pressed[pg.K_2]: selected = 'Koga'
@@ -251,15 +288,21 @@ def select_kanto_champs()->str:
         elif key_pressed[pg.K_4]: selected = 'Bruno'
         elif key_pressed[pg.K_5]: selected = 'Karen'
         check_quit()
-    #design screen displaying which team has been choosen
     return selected
 
-
-def select_starter(team: Team)->int:
+def select_starter(team1: Team, team2: Team)->int:
+    """
+    Enviroment for selecting the starting pokemon of self team, display and things to be drawn
+    """
     reset_screen()
     draw_bg('loading_bg.jpg')
-    display_message("SELECT YOUR STARTER POKEMON", (SIZE[0]//2, SIZE[1]//2-150), color=WHITE)
-    draw_pokemons_menu(team)
+    display_message("YOU ARE FIGHTING AGAINST", (SIZE[0]//2, SIZE[1]//2-150), color=WHITE, size=80)
+    draw_pokemons_menu(team2)
+    wait_for_no_keys()
+    wait_for_key_pressed()
+    draw_bg('loading_bg.jpg')
+    display_message("SELECT YOUR STARTER POKEMON", (SIZE[0]//2, SIZE[1]//2-150), color=WHITE, size=80)
+    draw_pokemons_menu(team1)
     wait_for_no_keys()
     selected = None
     while selected == None:
@@ -271,10 +314,16 @@ def select_starter(team: Team)->int:
         elif key_pressed[pg.K_5]: selected = 4
         elif key_pressed[pg.K_6]: selected = 5
         check_quit()
-    #blink the image of the selected pokemon
+    wait_for_no_keys()
+    draw_bg('loading_bg.jpg')
+    display_message("YOU START", (SIZE[0]//2, SIZE[1]//2), color=WHITE, size=100)
+    wait_for_key_pressed()
     return selected
 
 def fight(team1: Team, team2: Team):
+    """
+    Enviroment where the battle smulation takes place
+    """
     winner = None
     team_turn = 0
     while not winner:
@@ -298,21 +347,21 @@ def fight(team1: Team, team2: Team):
         elif  any(pokemon.current_hp > 0 for pokemon in team2.pokemons) and not any(pokemon.current_hp > 0 for pokemon in team1.pokemons): winner = team2
     return winner
 
-
 def display_winner(team: Team):
+    """
+    Shows the winner team on the same layout as the menu
+    """
     draw_bg('loading_bg.jpg')
     wait_for_key_pressed()
     wait_for_no_keys()
-    display_message("THE WINNER TEAM IS", (SIZE[0]//2, SIZE[1]//2), color=WHITE, rescale=2.5)
+    display_message("THE WINNER TEAM IS", (SIZE[0]//2, SIZE[1]//2), color=WHITE, size=120)
     wait_for_key_pressed()
     wait_for_no_keys()
     draw_bg('loading_bg.jpg')
-    display_message(team.name.upper(), (SIZE[0]//2, SIZE[1]//2-150), color=WHITE, rescale=2)
+    display_message(team.name.upper(), (SIZE[0]//2, SIZE[1]//2-150), color=WHITE, size=200)
     draw_pokemons_menu(team)
     wait_for_key_pressed()
     wait_for_no_keys()
-
-
 
 def main():
     reset_screen()
@@ -320,7 +369,7 @@ def main():
     selected_champ = select_kanto_champs()
     your_team = create_self_team('best_team.csv')
     op_team = team_por_nombre(selected_champ, KANTO_TEAMS[selected_champ])
-    starter_index = select_starter(your_team)
+    starter_index = select_starter(your_team, op_team)
     your_team.change_pokemon(starter_index)
     winner = fight(your_team, op_team)
     display_winner(winner)
