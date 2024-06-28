88b0f75a7d83e9811de2b6d2b9fcb6024b2c33d9
IgnacioTufaro <nacho.tufaro@gmail.com>
2024-06-28 13:36:05 -0300

    nuevo update

--- program/Main.py
+++ program/Main.py
@@ -8,20 +8,15 @@ from tqdm import tqdm
 import multiprocessing
 
 if __name__ == '__main__':
+    
     #Parametros iniciales
     poblacion_inicial = 50
     cant_rivales  = 400
     corte_seleccion = 25
-    generaciones = 25
+    generaciones = 50
     num_procesos = multiprocessing.cpu_count()
 
-#Parametros iniciales
-poblacion_inicial = 50
-cant_rivales  = 400
-corte_seleccion = 25
-generaciones = 50
 
-if __name__ == '__main__':
     #Extraigo la lista de pokemos y movimientos del arhivo CSV. Y la tabla de efectividad    
     lista_moves = lista_movimientos()
     pokemones = lista_pokemones()
@@ -41,17 +36,13 @@ if __name__ == '__main__':
     print("Comienza el algoritmo genetico")
     ultima_poblacion, ultimos_rivales, dict_vict_finales = algoritmo_completo(corte_seleccion,generaciones,lista_moves,pokemones,efectividad,poblacion,rivales)
 
-    #Cargo los datos de la ultima generacion
+
     dict_tipos = contar_frecuencia_tipos(ultima_poblacion)
     dict_tipos_ordenado = dict(sorted(dict_tipos.items(), key=lambda item: item[1], reverse=True))
-    cargar_tipos_en_csv(dict_tipos_ordenado,"Cantidad_tipo_ult_gen4.csv")
-#Cargo los datos de la ultima generacion
-dict_tipos = contar_frecuencia_tipos(ultima_poblacion)
-dict_tipos_ordenado = dict(sorted(dict_tipos.items(), key=lambda item: item[1], reverse=True))
-cargar_tipos_en_csv(dict_tipos_ordenado,"Cantidad_tipo_ult_gen.csv")
+    cargar_tipos_en_csv(dict_tipos_ordenado,"Cantidad_tipo_ult_gen.csv")
 
-#Cargo el mejor equipo para pelear en el simulador
-escritura_mejor_team('best_team.csv', dict_vict_finales)
+    #Cargo el mejor equipo para pelear en el simulador
+    escritura_mejor_team('best_team.csv', dict_vict_finales)
 
 
 
